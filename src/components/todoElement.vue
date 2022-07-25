48b502e3b99b9130ca885dcf55469493b38991b2
Sridharvn <sridharvnampoothiripad@gmail.com>
2022-07-25 23:53:00 +0530

    added todo functionalities and styling

--- src/components/todoElement.vue
+++ src/components/todoElement.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="todo">
     <h3>{{ todo.title }}</h3>
-    <h5>{{ todo.completed }}</h5>
+    <!-- <h5>{{ todo.completed }}</h5> -->
+    <button @click="$emit('completeTodo')" id="completeButton">
+      {{ todo.completed ? 'Completed' : 'Not Completed' }}
+    </button>
+    <button @click="$emit('removeTodo')">Remove</button>
   </div>
 </template>
 <script setup>
 // defineProps('todo', {
 //   title: String,
 //   completed: Boolean
+
+const { onMounted, watch } = require("@vue/runtime-core");
+
 // });
 const props = defineProps({
   todo: {
@@ -15,12 +22,38 @@ const props = defineProps({
     completed: Boolean
   }
 });
+const emit = defineEmits([
+  'removeTodo', 'completeTodo'
+]);
+// check if todo is completed and change the button color accordingly
+watch(props.completed, (newValue) => {
+  if (newValue) {
+    document.getElementById('completeButton').style.backgroundColor = 'green';
+  }
+  else {
+    document.getElementById('completeButton').style.backgroundColor = 'red';
+  }
+});
 </script>
 <style lang="scss" scoped>
 #todo {
-  background-color: #f0f0f0;
+  background-color: $background;
   padding: 20px;
   margin: 10px;
   border-radius: 5px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  border: $secondary 1px solid;
+  color: $textcolor;
+}
+
+button {
+  padding: 10px;
+  border-radius: 5px;
+  background-color: $background;
+  border: 1px solid $secondary;
+  color: $textcolor;
 }
 </style>
--- src/pages/TodosPage.vue
+++ src/pages/TodosPage.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
     <h1>Todos</h1>
-    <div v-for="todo in todoList" :key="todo">
-      <TodoElement :todo="todo" />
+    <div id="addTodo">
+      <input type="text" v-model="newTodo" placeholder="Add a new todo" v-on:keyup.enter="addTodo" id="newTodoInput" />
+      <span><button @click="addTodo" id="addButton">Add</button></span>
+    </div>
+    <div v-for="(todo, index) in todoList.slice().reverse()" :key="todo" id="todoList">
+      <TodoElement :todo="todo" @completeTodo="completeTodo(index)" @removeTodo="removetodo(index)" />
     </div>
   </div>
 </template>
 <script setup>
+import { watch } from 'vue';
 import TodoElement from '../components/todoElement.vue'
-const { reactive } = require("@vue/reactivity");
+import { useQuasar } from 'quasar';
+const { reactive, ref } = require("@vue/reactivity");
 
+
+
+const $q = useQuasar();
+const newTodo = ref('');
 const todoList = reactive([
   {
     id: 1,
@@ -27,9 +37,95 @@ const todoList = reactive([
     completed: false
   }
 ]);
+
+function addTodo() {
+  if (newTodo.value.length > 0) {
+    todoList.push({
+      id: todoList.length + 1,
+      title: newTodo.value,
+      completed: false
+    });
+    newTodo.value = '';
+    $q.notify({
+      message: 'Task added',
+      color: 'secondary'
+    })
+  }
+  else {
+    $q.notify({
+      message: 'Task cannot be empty',
+      color: 'negative'
+    })
+  }
+}
+function completeTodo(index) {
+  const trueIndex = todoList.length - index - 1;
+  // console.log(todoList[trueIndex]);
+  todoList[trueIndex].completed = !todoList[trueIndex].completed;
+  if (todoList[trueIndex].completed) {
+    $q.notify({
+      message: 'Task completed',
+      color: 'positive'
+    })
+  }
+  else {
+    $q.notify({
+      message: 'Task not completed',
+      color: 'negative'
+    })
+  }
+}
+function removetodo(index) {
+  const trueIndex = todoList.length - index - 1;
+  // console.log(todoList[trueIndex]);
+  if (!todoList[trueIndex].completed) {
+    $q.notify({
+      message: 'Incomplete task cannot be removed',
+      color: 'negative'
+    })
+  }
+  else {
+    todoList.splice(trueIndex, 1);
+    $q.notify({
+      message: 'Task removed',
+      color: 'negative'
+    })
+  }
+}
 </script>
 <style lang="scss" scoped>
 h1 {
   text-align: center;
+  color: $textcolor;
 }
+
+input {
+  width: 80%;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid $secondary;
+  background-color: $background;
+  color: $textcolor;
+}
+
+button {
+  padding: 10px;
+  border-radius: 5px;
+  background-color: $background;
+  border: 1px solid $secondary;
+  color: $textcolor;
+}
+
+#addTodo {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin: 20px;
+}
+
+// #newTodoInput {
+//   width: 100%;
+//   margin-bottom: 10px;
+// }
 </style>
