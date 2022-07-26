0d9facaa36803dd518f47e12d6b85229a45cdb43
Sridharvn <sridharvnampoothiripad@gmail.com>
2022-07-26 19:16:20 +0530

    Set up basic crud with firestore

--- src/pages/TodosPage.vue
+++ src/pages/TodosPage.vue
@@ -5,16 +5,17 @@
       <input type="text" v-model="newTodo" placeholder="Add a new todo" v-on:keyup.enter="addTodo" id="newTodoInput" />
       <span><button @click="addTodo" id="addButton">Add</button></span>
     </div>
-    <div v-for="(todo, index) in todoList.slice().reverse()" :key="todo" id="todoList">
-      <TodoElement :todo="todo" @completeTodo="completeTodo(index)" @removeTodo="removetodo(index)" :key="todo.id" />
+    <div v-for="(todo) in todoList.slice().reverse()" :key="todo" id="todoList">
+      <TodoElement :todo="todo" @completeTodo="completeTodo(todo.id)" @removeTodo="removetodo(todo.id)"
+        :key="todo.id" />
     </div>
   </div>
 </template>
 <script setup>
 import { onMounted, watch } from 'vue';
 import TodoElement from '../components/todoElement.vue'
-import { useQuasar } from 'quasar';
-import { collection, getDocs } from "firebase/firestore";
+import { date, useQuasar } from 'quasar';
+import { collection, onSnapshot, addDoc, deleteDoc, doc, updateDoc, query, orderBy, limit } from "firebase/firestore";
 import { db } from '../firebase';
 
 
@@ -24,96 +25,103 @@ const { reactive, ref } = require("@vue/reactivity");
 
 const $q = useQuasar();
 const newTodo = ref('');
-const todoList = reactive([
-  // {
-  //   id: 1,
-  //   title: "Learn Vue",
-  //   completed: false
-  // },
-  // {
-  //   id: 2,
-  //   title: "Learn Vuex",
-  //   completed: false
-  // },
-  // {
-  //   id: 3,
-  //   title: "Learn Vue Router",
-  //   completed: false
+const todoList = ref([]);
+const todosref = collection(db, "todos");
+const todoscollectionquery = query(todosref, orderBy("date", "asc"));
+async function addTodo() {
+  // if (newTodo.value.length > 0) {
+  //   todoList.push({
+  //     id: todoList.length + 1,
+  //     title: newTodo.value,
+  //     completed: false
+  //   });
+  //   newTodo.value = '';
+  //   $q.notify({
+  //     message: 'Task added',
+  //     color: 'secondary'
+  //   })
   // }
-]);
-
-function addTodo() {
-  if (newTodo.value.length > 0) {
-    todoList.push({
-      id: todoList.length + 1,
-      title: newTodo.value,
-      completed: false
-    });
-    newTodo.value = '';
-    $q.notify({
-      message: 'Task added',
-      color: 'secondary'
-    })
-  }
-  else {
-    $q.notify({
-      message: 'Task cannot be empty',
-      color: 'negative'
-    })
-  }
+  // else {
+  //   $q.notify({
+  //     message: 'Task cannot be empty',
+  //     color: 'negative'
+  //   })
+  // }
+  await addDoc(todosref, {
+    title: newTodo.value,
+    completed: false,
+    date: Date.now()
+  });
+  newTodo.value = '';
 }
-function completeTodo(index) {
-  const trueIndex = todoList.length - index - 1;
-  // console.log(todoList[trueIndex]);
-  todoList[trueIndex].completed = !todoList[trueIndex].completed;
-  if (todoList[trueIndex].completed) {
-    $q.notify({
-      message: 'Task completed',
-      color: 'positive'
-    })
-  }
-  else {
-    $q.notify({
-      message: 'Task not completed',
-      color: 'negative'
-    })
-  }
+async function completeTodo(id) {
+  // const trueIndex = todoList.length - index - 1;
+  // todoList[trueIndex].completed = !todoList[trueIndex].completed;
+  // if (todoList[trueIndex].completed) {
+  //   $q.notify({
+  //     message: 'Task completed',
+  //     color: 'positive'
+  //   })
+  // }
+  // else {
+  //   $q.notify({
+  //     message: 'Task not completed',
+  //     color: 'negative'
+  //   })
+  // }
+  const index = todoList.value.findIndex(todo => todo.id === id);
+  await updateDoc(doc(db, "todos", id), {
+    completed: !todoList.value[index].completed
+  });
 }
-function removetodo(index) {
-  const trueIndex = todoList.length - index - 1;
-  // console.log(todoList[trueIndex]);
-  if (!todoList[trueIndex].completed) {
-    $q.notify({
-      message: 'Incomplete task cannot be removed',
-      color: 'negative'
-    })
-  }
-  else {
-    todoList.splice(trueIndex, 1);
-    $q.notify({
-      message: 'Task removed',
-      color: 'negative'
-    })
-  }
+function removetodo(id) {
+  // const trueIndex = todoList.length - index - 1;
+  // // (todoList[trueIndex]);
+  // if (!todoList[trueIndex].completed) {
+  //   $q.notify({
+  //     message: 'Incomplete task cannot be removed',
+  //     color: 'negative'
+  //   })
+  // }
+  // else {
+  //   todoList.splice(trueIndex, 1);
+  //   $q.notify({
+  //     message: 'Task removed',
+  //     color: 'negative'
+  //   })
+  // }
+  deleteDoc(doc(db, "todos", id));
 }
 
 // Get todos on Mounted
 onMounted(async () => {
-  const querySnapshot = await getDocs(collection(db, "todos"));
-  let allTodos = [];
-  querySnapshot.forEach((doc) => {
-    let fetchedTodo = {
-      id: doc.id,
-      title: doc.data().title,
-      completed: doc.data().completed
-    }
-    allTodos.push(fetchedTodo);
-    console.log(doc.id, " => ", doc.data());
+  // const querySnapshot = await getDocs(collection(db, "todos"));
+  // let allTodos = [];
+  // querySnapshot.forEach((doc) => {
+  //   let fetchedTodo = {
+  //     id: doc.id,
+  //     title: doc.data().title,
+  //     completed: doc.data().completed
+  //   }
+  //   allTodos.push(fetchedTodo);
+  // });
+  // todoList.value = allTodos;
+
+  onSnapshot(todoscollectionquery, (querySnapshot) => {
+    const allTodos = [];
+    querySnapshot.forEach((doc) => {
+      let fetchedTodo = {
+        id: doc.id,
+        title: doc.data().title,
+        completed: doc.data().completed
+      }
+      allTodos.push(fetchedTodo);
+    });
+    todoList.value = allTodos;
   });
-  console.log(allTodos);
-  todoList.value = allTodos;
-  console.log(todoList);
-})
+});
+
+
 
 
 </script>
